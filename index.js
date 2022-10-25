387817b1b4a5d6816eab066c4fe7dc419b5b6d47
Dipti saha <sahadipti322@gmail.com>
2022-10-24 20:15:28 -0400

    filter catagory data by catagory id

--- index.js
+++ index.js
@@ -2,13 +2,19 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
-const catagory = require('./data/catagory.json')
+const catagory = require('./data/catagory.json');
+const courses=require('./data/courses.json');
 app.use(cors());
 app.get('/', (req, res) => {
     res.send('Courses API Running');
 });
 app.get('/courses/catagory', (req, res) => {
     res.send(catagory); 
+});
+app.get('/courses/:id', (req, res) => {
+   const id= req.params.id;
+   const catagory_courses = courses.filter(c => c.catagory_id === id); 
+   res.send(catagory_courses);
 });
 app.listen(port, () => {
     console.log('Dragon News Server running on port', port);
